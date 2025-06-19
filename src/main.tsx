f43233c3bb4672a04dc93583c9f9c5a849810cba
Faraz Shaikh <farazzshaikh@gmail.com>
2025-06-19 18:11:21 +0400

    init

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+import { UI } from "./Ui";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+  <>
+    <App />
+    <UI />
+  </>
+);
